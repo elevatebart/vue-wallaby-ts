00d3e922debde49ccd391749e35c820b1f9a0de7
Artem Govorov <artem.govorov@gmail.com>
2017-12-03 13:59:31 +1000

    Update wallaby.js

--- wallaby.js
+++ wallaby.js
@@ -2,21 +2,26 @@ const wallabyWebpack = require('wallaby-webpack')
 var webpackConfig = require('./build/webpack.test.conf')
 
 module.exports = function (wallaby) {
-  webpackConfig.resolve.alias = {'@': require('path').join(wallaby.projectCacheDir, 'src')}
-  webpackConfig.module.rules.find(r => r.loader === 'vue-loader').options.loaders.jss = ''
+  webpackConfig.resolve.alias = {'@': require('path').join(wallaby.projectCacheDir, 'src'), 'vue$': 'vue/dist/vue.esm.js'}
+  webpackConfig.module.rules = webpackConfig.module.rules.filter(r => !'.ts'.match(r.test) && !'.js'.match(r.test));
+  webpackConfig.module.rules.find(r => r.loader === 'vue-loader').options.loaders.js = '';
+
   const wallabyPostprocessor = wallabyWebpack(webpackConfig)
 
   return {
     files: [
       {pattern: 'node_modules/chai/chai.js', instrument: false},
-      {pattern: 'node_modules/sinon/lib/sinon.js', instrument: false},
+      {pattern: 'node_modules/sinon/pkg/sinon.js', instrument: false},
       {pattern: 'src/**/*.*', load: false},
     ],
 
     compilers: {
-      '**/*.vue': require('wallaby-vue-compiler')(wallaby.compilers.typeScript({})),
       '**/*.js': wallaby.compilers.babel(),
-      '**/*.ts': wallaby.compilers.typeScript()
+      '**/*.ts': wallaby.compilers.typeScript({module: 'CommonJs', target: 'ES5'})
+    },
+
+    preprocessors: {
+      '**/*.vue': f => f.content.replace('lang="ts"', '').replace('.ts', '.js')
     },
 
     tests: [
